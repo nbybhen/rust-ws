2091f268e5fd340eb35b833b5424593385463a83
Henock Dinberu <henockdinberu11@gmail.com>
2024-09-13 23:41:02 -0400

    Added Python support!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use axum::routing::get;
 use socketioxide::{
     extract::SocketRef,
@@ -7,29 +8,75 @@ use socketioxide::extract::TryData;
 use socketioxide::socket::Socket;
 use tower::ServiceBuilder;
 use tower_http::cors::CorsLayer;
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use std::sync::OnceLock;
+use serde::ser::SerializeMap;
+use serde_with::serde_as;
+use std::process::Command;
+use futures_util::stream::iter;
+use shutil::pipe;
+use std::fs;
 
 extern crate log;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct LangInfo {
+    name: &'static str,
+    cmds: Box<[&'static str]>,
+    ext: &'static str
+}
+
+impl LangInfo {
+    fn new(name: &'static str, cmds: Box<[&'static str]>, ext: &'static str) -> Self {
+        Self {name, cmds, ext}
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Code {
+    lang: String,
+    code: String,
+}
 
 pub struct Session {
     socket: SocketRef,
     data: Code,
     is_ide: bool,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Code {
-    lang: String,
-    code: String
+    languages: OnceLock<HashMap<&'static str, LangInfo>>,
 }
 
 impl Session {
     pub fn new(socket: SocketRef, data: Code, is_ide: bool) -> Self {
-        Self {socket, data, is_ide}
+        let cell = OnceLock::new();
+        cell.set({
+            let mut hash: HashMap<&'static str, LangInfo> = HashMap::new();
+            hash.insert("python", LangInfo::new("python3", Box::new(["/C", "python3 src/tmp/main.py"]), ".py"));
+            hash
+        }).expect("Unable to set OnceLock");
+
+        Self {socket, data, is_ide, languages: cell}
     }
 
     pub fn run_ide(&self) {
+        self.socket.emit("message-back", "Running code...").ok();
+        if let Some(lang) = self.languages.get().unwrap().get(self.data.lang.as_str()) {
+            log::debug!("Chosen: {:?}", lang.name);
+            let dir = format!("src/tmp/main{}", lang.ext);
+            log::debug!("File location: {dir:?}");
+            fs::write(dir, &self.data.code).expect("Failed to write to file.");
 
+            let child = Command::new("cmd")
+                .args(&*lang.cmds)
+                .stderr(std::process::Stdio::null())
+                .stdout(std::process::Stdio::piped())
+                .stdin(std::process::Stdio::piped())
+                .spawn().expect("Could not run the command(s)");
+
+            let output = child.wait_with_output().expect("Could not wait for child process");
+            let f_output = output.stdout.iter().map(|&x| x as char).collect::<String>();
+            log::debug!("{:?}", f_output);
+            self.socket.emit("response", f_output).unwrap();
+        }
     }
 }
 
@@ -41,14 +88,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     std::env::set_var("RUST_LOG", "debug");
     env_logger::init();
 
-    // Register a handler for the default namespace
     io.ns("/", |s: SocketRef| {
         log::debug!("Connected! {}", s.id);
         s.on("message", |s: SocketRef, TryData::<Code>(data)| {
-            println!("Received message: {:?}", data.ok());
-            if let Some(code) = data.clone() {
-                s.emit("message-back", "Running code...").ok();
+            if let Some(code) = data.ok() {
+                println!("Received message: {:?}", code);
                 let response = Session::new(s, code, true);
+                response.run_ide();
+            }
+            else {
+                log::error!("Failed to process message!");
+                s.emit("message-back", "Failed to process message!").ok();
             }
 
         });
